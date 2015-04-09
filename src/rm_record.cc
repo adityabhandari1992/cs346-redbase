2bb96bdc32dc36373102ec77e6ac90cedfd408a4
Aditya Bhandari <adityabhandari1992@gmail.com>
2015-04-09 03:56:22 -0700

    Implemented RM Record class

--- src/rm_record.cc
+++ src/rm_record.cc
@@ -10,20 +10,40 @@ using namespace std;
 
 // Default constructor
 RM_Record::RM_Record() {
-
+    // Set the valid flag to false
+    this->isValid = FALSE;
 }
 
 // Destructor
 RM_Record::~RM_Record() {
-
+    // Delete the RID
+    delete rid;
 }
 
 // Return the data corresponding to the record
 RC RM_Record::GetData(char *&pData) const {
+    // Check if the record is valid
+    if (!isValid) {
+        return RM_RECORD_NOT_VALID;
+    }
 
+    // Point pData to the data in the record
+    pData = this->pData;
+
+    // Return OK
+    return OK_RC;
 }
 
 // Return the RID associated with the record
 RC RM_Record::GetRid (RID &rid) const {
+    // Check if the record is valid
+    if (!isValid) {
+        return RM_RECORD_NOT_VALID;
+    }
 
+    // Point rid to the RID in the record
+    rid = this->rid;
+
+    // Return OK
+    return OK_RC;
 }
