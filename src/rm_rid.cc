8958e14c2e4887651ee527a757e2d6766d6921ff
Aditya Bhandari <adityabhandari1992@gmail.com>
2015-04-08 19:20:40 -0700

    Add the RID class

---
+++ src/rm_rid.cc
@@ -0,0 +1,82 @@
+//
+// File:        rm_rid.cc
+// Description: RID class implementation
+// Authors:     Aditya Bhandari (adityasb@stanford.edu)
+//
+
+#include "rm_rid.h"
+using namespace std;
+
+// Default constructor
+RID::RID() {
+    // Set the viable flag to false
+    this->isViable = false;
+}
+
+// Constructor with PageNum and SlotNum given
+RID::RID(PageNum pageNum, SlotNum slotNum) {
+    // Copy the page number and slot number into the local variables
+    this->pageNumber = pageNum;
+    this->slotNumber = slotNum;
+
+    // Set the viable flag to true
+    this->isViable = true;
+}
+
+// Destructor
+RID::~RID() {
+    // Don't need to do anything
+}
+
+// Copy constructor
+RID::RID(const RID &rid) {
+    // Copy the page number, slot number and viable flag
+    this->pageNumber = rid.pageNumber;
+    this->slotNumber = rid.slotNumber;
+    this->isViable = rid.isViable;
+}
+
+// Overload =
+RID& RID::operator=(const RID &rid) {
+    // Check for self-assignment
+    if (this != &pageHandle) {
+        // Copy the page number, slot number and viable flag
+        this->pageNumber = rid.pageNumber;
+        this->slotNumber = rid.slotNumber;
+        this->isViable = rid.isViable;
+    }
+
+    // Return a reference to this
+    return (*this);
+}
+
+
+// Return page number
+RC RID::GetPageNum(PageNum &pageNum) const {
+    // If the RID is not viable, return a positive error
+    if (!isViable) {
+        return RID_NOT_VIABLE;
+    }
+    else {
+        // Set pageNum to the page number of the RID
+        pageNum = this->pageNumber;
+
+        // Return OK
+        return OK_RC;
+    }
+}
+
+// Return slot number
+RC RID::GetSlotNum(SlotNum &slotNum) const {
+    // If the RID is not viable, return a positive error
+    if (!isViable) {
+        return RID_NOT_VIABLE;
+    }
+    else {
+        // Set slotNum to the slot number of the RID
+        slotNum = this->slotNumber;
+
+        // Return OK
+        return OK_RC;
+    }
+}
